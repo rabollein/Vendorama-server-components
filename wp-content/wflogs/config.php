8723fc234c92f7ad8f052444a8ef836b9b123f5c
Ralf Bolleininger <r.bolleininger@gmail.com>
2026-03-18 07:16:51 +0100

    chore: wflogs aus Git-Tracking entfernt

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1773757483;s:7:"authKey";s:64:"hJ@YpH]0$VfShkzD=}=(-AS#Y&ut,w2zKnq~W{hxr--K/lJT)tF3SR,20[}b4KI1";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2968;}
